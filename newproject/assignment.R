f650b8aa79285c69a5376e95a7779b9400ea7d3b
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-12-26 19:01:58 +0800

    Update assignment.R
    Added cleaning for $Price column for dataSet

--- newproject/assignment.R
+++ newproject/assignment.R
@@ -4,8 +4,24 @@
 # 3. Lim Ee Chian TP065138
 # 4. Yong Jie Yee TP078458
 
-Objective
-#Step 1: Importing the csv file as dataSet
+# Objectives
+# Step 1: Importing the csv file as dataset
+
+# Ensuring that you extract the 
 dataSet = read.csv("kl_property_data.csv")
 
 #Step 2: Cleaning the data
+
+
+##Converting the $Price column into int variable type
+
+  #Removing the RM heading
+  dataSet$Price <- gsub("RM", "", dataSet$Price)
+  
+  #Removing any white-spaces
+  dataSet$Price <- gsub("\\s", "", dataSet$Price)
+  
+  #Removing commas between the values and turning it into numerical variable type
+  dataSet$Price <- as.numeric(gsub(",", "", df$Price))
+  
+#
