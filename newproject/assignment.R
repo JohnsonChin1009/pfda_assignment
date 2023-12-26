79293941864eecda27c6e4c6d392955d82889a84
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-12-26 15:06:58 +0800

    Update assignment.R

--- newproject/assignment.R
+++ newproject/assignment.R
@@ -4,7 +4,8 @@
 # 3. Lim Ee Chian TP065138
 # 4. Yong Jie Yee TP078458
 
-#Step 1: Importing the dataset
-dataSet = read.csv("/kl_property_data.csv")
+Objective
+#Step 1: Importing the csv file as dataSet
+dataSet = read.csv("kl_property_data.csv")
 
-# C:\Users\shado\Desktop\pfda_assignment\newproject
+#Step 2: Cleaning the data
