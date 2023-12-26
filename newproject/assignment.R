fff50714ec17999e42c7fddd1ddbc9e87239da85
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-12-26 15:05:43 +0800

    Added kl_property_data.csv file and amended assignment.R file

--- newproject/assignment.R
+++ newproject/assignment.R
@@ -1,5 +1,10 @@
 # Group
 # 1. Chin Hong Wei TP065390
-# 2. Looi Wei En TP065181
+# 2. Loo Hui En TP065181
 # 3. Lim Ee Chian TP065138
 # 4. Yong Jie Yee TP078458
+
+#Step 1: Importing the dataset
+dataSet = read.csv("/kl_property_data.csv")
+
+# C:\Users\shado\Desktop\pfda_assignment\newproject
